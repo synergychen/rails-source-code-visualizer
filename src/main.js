8c5a52db3859633d3ef3ce943995b70e5ae889b2
ChenHuang <alan.tolearn@gmail.com>
2018-08-05 23:41:48 -0400

    Add vue awesome icon globally

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
+import 'vue-awesome/icons'
+import Icon from "vue-awesome/components/Icon.vue";
 import App from './App.vue'
 
+Vue.component('icon', Icon)
 Vue.config.productionTip = false
 
 new Vue({
